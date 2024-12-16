2ebc5addd18876176c8a21497ac3b777c75f94ef
JAMunDo <brishnel@gmail.com>
2024-12-15 22:52:54 -0500

    Update App.java

--- src/main/java/com/tech/novus/App.java
+++ src/main/java/com/tech/novus/App.java
@@ -3,6 +3,6 @@ package com.tech.novus;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Compuzign CLI");
-        MainProgram.mainMenu();
+        MainProgram.login();
     }
 }
